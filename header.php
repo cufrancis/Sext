14a2a2597f41d709a5189d64dbe98345444d7ff1
cufrancis <cufrancis.com@gmail.com>
2016-06-10 16:02:41 +0800

    add meta

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="zh-CN">
 <head>
 	<meta charset="<?php $this->options->charset(); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php $this->archiveTitle(' &raquo; ', '', ' - '); ?><?php $this->options->title(); ?></title>
 	<!-- 新 Bootstrap 核心 CSS 文件 -->
 	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
